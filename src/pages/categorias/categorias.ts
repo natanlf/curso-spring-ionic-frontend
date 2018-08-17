a9cb65e1b1fc1beb4e4098cfe6e5ba58e9b51618
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-08-17 11:11:20 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,4 @@
+import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +16,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //coloco categoriaService no construtor para poder fazer o request e pegas a lista de categorias
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) { 
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+  //quando a página terminar de ser carregada, executa o que tiver dentor do método
+  ionViewDidLoad() { 
+    //como é uma chamada assincrona, tenho que me inscrever, então uso o subscribe
+    //dentro do subscribe() coloco uma função para ser executada quando a resposta chegar
+    //essa função é a famosa "callback"
+    //vamos usar uma função anonima, que é também chamada de arrow function
+    this.categoriaService.findAll()
+      .subscribe(response =>{ //com arrow function, declarei uma função dentro de outra
+        console.log(response); //tenho duas funções dentro da função subscribe, a response e a error
+      },
+    error => {
+      console.log(error);
+    });
+    console.log();
   }
-
 }
